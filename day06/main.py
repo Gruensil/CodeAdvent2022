647f313ea331f337ad6c8c2931fa5bac48f8a5d7
Silas Grün <gruensil@gmail.com>
2022-12-06 22:31:24 +0100

    make number general

--- day06/main.py
+++ day06/main.py
@@ -1,24 +1,27 @@
 import time
 
-def check_four_chars(input, idx):
-    for ix, charx in enumerate(input[idx:idx+3]):
-        for iy, chary in enumerate(input[idx+ix+1:idx+4]):
+n = 4
+
+def check_n_chars(input, idx):
+    for ix, charx in enumerate(input[idx:idx+n-1]):
+        for iy, chary in enumerate(input[idx+ix+1:idx+n]):
             if charx == chary:
                 return False
     return True
 
-def check_input(input):
-    for idx in range(len(input)-4):
-        if check_four_chars(input, idx):
+def check_input_for_n(input):
+    for idx in range(len(input)-n):
+        if check_n_chars(input, idx):
             return idx
     return False
 
 with open('C:\\Users\\gruen\\Documents\\CodeAdvent2022\\day06\\input.txt') as f:
     line = f.readline()
     tic = time.perf_counter()
-    idx = check_input(line)
+    n = 14
+    idx = check_input_for_n(line)
     toc = time.perf_counter()
-    print (f"4 individual chars found at pos {idx}!")
-    print (f"Therefor the result is {idx + 4}")
+    print (f"{n} individual chars found at pos {idx}!")
+    print (f"Therefor the result is {idx + n}")
     print(f"Found solution in {toc - tic:0.4f} seconds") # 0.0012s
 
