dae807985a74459c95a0205226a01c3f6a2cc732
Silas Grün <gruensil@gmail.com>
2022-12-06 22:08:41 +0100

    naive solution for first part

---
+++ day06/main.py
@@ -0,0 +1,24 @@
+import time
+
+def check_four_chars(input, idx):
+    for ix, charx in enumerate(input[idx:idx+3]):
+        for iy, chary in enumerate(input[idx+ix+1:idx+4]):
+            if charx == chary:
+                return False
+    return True
+
+def check_input(input):
+    for idx in range(len(input)-4):
+        if check_four_chars(input, idx):
+            return idx
+    return False
+
+with open('C:\\Users\\gruen\\Documents\\CodeAdvent2022\\day06\\input.txt') as f:
+    line = f.readline()
+    tic = time.perf_counter()
+    idx = check_input(line)
+    toc = time.perf_counter()
+    print (f"4 individual chars found at pos {idx}!")
+    print (f"Therefor the result is {idx + 4}")
+    print(f"Found solution in {toc - tic:0.4f} seconds")
+
